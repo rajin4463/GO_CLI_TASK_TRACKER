188e6750a960081b7b59df5542153b09c7099a92
rajin4463 <73782473+rajin4463@users.noreply.github.com>
2025-05-06 12:48:59 +0530

    Delete command half implement

--- utils/csvFunc.go
+++ utils/csvFunc.go
@@ -2,18 +2,21 @@ package utils
 
 import (
 	"encoding/csv"
-	"os"
+	"strconv"
+	"fmt"
 	"log"
+	"os"
 )
 
 func CsvRead(fileName string) [][]string {
 	tasks, _ := os.Open(fileName)
 	task, err := csv.NewReader(tasks).ReadAll()
-	if err != nil {}
+	if err != nil {
+	}
 	return task
 }
 
-func CsvWrite(fileName string, data []string){
+func CsvWrite(fileName string, data []string) {
 	csvFile, err := os.Create(fileName)
 	if err != nil {
 		log.Fatalf("Failed creating file: %s", err)
@@ -22,7 +25,19 @@ func CsvWrite(fileName string, data []string){
 
 	csvwriter := csv.NewWriter(csvFile)
 
-	for _, task := range data{
+	for _, task := range data {
 		_ = csvwriter.Write([]string{task})
+	}
+}
+
+func CsvDel(fileName string, id int) {
+	data := CsvRead(fileName)
+	var NewData [][]string
+	for i := 1; i < len(data); i++ { // Start from 1 to skip the header
+		fmt.Println(data[i])
+		Listid, _ := strconv.Atoi(data[i][0])
+		if Listid == id{
+			
+		}
 	}
 }
